7ca695fdef4ff2dc7d91908c7e4c6b1b04037bcf
Maciej Szankin <maciej@szankin.pl>
2020-08-05 17:49:10 -0700

    Add script to generate student accounts

---
+++ hub/generate_students.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Usage: ./generate_students.sh <NUMBER> to generate <NUMBER> of students accounts."
+    exit 1
+fi
+
+NUM=$1
+
+function getPswd {
+    tr -cd '[:alnum:]' < /dev/urandom | fold -w8 | head -n1
+}
+
+rm -f students.txt
+
+for i in $(seq 1 $NUM);
+do
+    USER=$(printf "%03d" $i)
+    PASS=$(getPswd);
+    echo $USER:$PASS | tee -a students.txt
+done;
