9b2db5809c9a9a942ef9f23d91b5344efe0307e9
Maciej Szankin <maciej@szankin.pl>
2020-08-05 19:38:30 -0700

    Mount home dir as volume

--- hub/run.sh
+++ hub/run.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+VOLUME_HOME=/data/issdl2020/home
+
+mkdir -p $VOLUME_HOME
+
 docker run \
     --hostname=DLHub \
+    -v $VOLUME_HOME:/home/ \
     -p 80:8000 \
     -p 9000-9500:9000-9500 \
     -ti dlhub/hub $@
-
-# /opt/conda/envs/dl/bin/jupyterhub
